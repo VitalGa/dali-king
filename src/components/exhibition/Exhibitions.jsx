ee02f59fdf4579d2eaaf1aaebeb9c69e8e71829d
VitalGa <vitalga10@gmail.com>
2024-03-01 12:05:46 +0300

    added Exhibitions

---
+++ src/components/exhibition/Exhibitions.jsx
@@ -0,0 +1,14 @@
+import styles from './Exhibitions.module.css'; 
+
+const Exhibitions = () => {
+  return (
+    <div className={styles['wrapper']}>
+      <h2>Онлайн-выставки</h2>
+      <div className={styles['wrapper-iner']}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Exhibitions;
--- src/pages/exhibition/Exhibitions.jsx
+++ src/pages/exhibition/Exhibitions.jsx
@@ -2,9 +2,30 @@ import styles from './Exhibitions.module.css';
 
 const Exhibitions = () => {
   return (
-    <>
-      Выставки
-    </>
+    <div className={styles['wrapper']}>
+      <div className={styles['container']}>
+        <div className={styles['text']}>
+        Онлайн-выставки
+      </div>
+          <div className={styles['row']}>
+            <div className={styles['dali-left']}></div>
+            <div className={styles['dali-right']}> <span>САЛЬВАДОР<br />ДАЛИ</span></div>
+          </div>
+          <div className={styles['row']}>
+            <div className={styles['kalo-left']}>ФРИДА КАЛО</div>
+            <div className={styles['kalo-right']}></div>
+          </div>
+          <div className={styles['row']}>
+            <div className={styles['magrit-left']}></div>
+            <div className={styles['magrit-right']}>РЕНЕ МАГРИТТ</div>
+          </div>
+      </div>
+      <div className={styles['all']}>
+        <img src="/arrow.png" alt="Переход на все выставки" />
+        <div className={styles['text-bottom']}><span>Посмотреть все выставки</span></div>
+      </div>
+      
+    </div>
   )
 }
 
