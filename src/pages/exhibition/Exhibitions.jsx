f232da723f4bd049940f1ed64568bdc439e14790
VitalGa <vitalga10@gmail.com>
2024-03-01 14:53:03 +0300

    anded Exhibitions

--- src/pages/exhibition/Exhibitions.jsx
+++ src/pages/exhibition/Exhibitions.jsx
@@ -1,3 +1,4 @@
+import Button from '../../components/button/Button';
 import styles from './Exhibitions.module.css'; 
 
 const Exhibitions = () => {
@@ -8,15 +9,49 @@ const Exhibitions = () => {
         Онлайн-выставки
       </div>
           <div className={styles['row']}>
-            <div className={styles['dali-left']}></div>
+            <div className={styles['dali-left']}>
+              <div className={styles['content']}>
+                <div className={styles['top']}>
+                  <div className={styles['name']}>Король <br /> сюрреализма</div>
+                  <div className={styles['date']}>САЛЬВАДОР ДАЛИ <br /> 10.08.24—10.08.25</div>
+                </div>
+                <div className={styles['bottom']}>
+                  <div className={styles['price']}>от 400 руб. / 120 мин</div>
+                  <Button >Выбрать выставку</Button>
+                </div>
+              </div>
+              
+            </div>
             <div className={styles['dali-right']}> <span>САЛЬВАДОР<br />ДАЛИ</span></div>
           </div>
           <div className={styles['row']}>
             <div className={styles['kalo-left']}>ФРИДА КАЛО</div>
-            <div className={styles['kalo-right']}></div>
+            <div className={styles['kalo-right']}>
+            <div className={styles['content']}>
+                <div className={styles['top']}>
+                  <div className={styles['name']}>VIVA LA VIDA</div>
+                  <div className={styles['date']}>ФРИДА КАЛО  И <br /> ДИЕГО РИВЕРА  <br /> 10.08.25—10.08.25</div>
+                </div>
+                <div className={styles['bottom']}>
+                  <div className={styles['price']}>от 400 руб. / 120 мин</div>
+                  <Button >Выбрать выставку</Button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles['row']}>
-            <div className={styles['magrit-left']}></div>
+            <div className={styles['magrit-left']}>
+            <div className={styles['content']}>
+                <div className={styles['top']}>
+                  <div className={styles['name']}>За гранью <br />реальности</div>
+                  <div className={styles['date']}>РЕНЕ МАГРИТТ <br /> 10.08.24—10.08.25</div>
+                </div>
+                <div className={styles['bottom']}>
+                  <div className={styles['price']}>от 400 руб. / 120 мин</div>
+                  <Button >Выбрать выставку</Button>
+                </div>
+              </div>
+            </div>
             <div className={styles['magrit-right']}>РЕНЕ МАГРИТТ</div>
           </div>
       </div>
